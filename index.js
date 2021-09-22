8cc4bda950534346a02c7bc974e9373f97e128ac
Josef Reichelt <pepareichelt@gmail.com>
2021-09-22 18:14:06 +0200

    Prettier matrix

--- index.js
+++ index.js
@@ -1,56 +1,66 @@
-const alphabetBig = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const alphabetBig = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const alphabetSmall = alphabetBig.toLowerCase();
-const numbers = "0123456789"
-const katakana =  "ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍｦｲｸｺｿﾁﾄﾉﾌﾔﾖﾙﾚﾛﾝ"
+const numbers = "0123456789";
+const katakana = "ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍｦｲｸｺｿﾁﾄﾉﾌﾔﾖﾙﾚﾛﾝ";
 const chars = alphabetBig + alphabetSmall + numbers + katakana;
 
-
 const canvasElement = document.getElementById("matrix");
-const context = canvasElement.getContext('2d');
+const context = canvasElement.getContext("2d");
 
 canvasElement.width = window.innerWidth;
 canvasElement.height = window.innerHeight;
 
 const fontSize = 16;
-const columns = Math.floor(canvasElement.width/fontSize);
-const rows = Math.floor(canvasElement.height/fontSize);
+const columns = Math.floor(canvasElement.width / fontSize);
+// const columns = 2;
+const rows = Math.floor(canvasElement.height / fontSize);
 
-console.log("Wake up, Neo...")
-console.log(columns,rows)
-console.log(canvasElement)
+console.log("Wake up, Neo...");
+console.log(columns, rows);
+console.log(canvasElement);
 
-const rainDrops = new Array(columns).fill(rows + 1);
+const rainDrops = new Array(columns).fill(0).map(() => ({
+  idx: rows + 2,
+  currentChar: "",
+  prevChar: "",
+}));
 
-console.log(rainDrops)
+console.log(rainDrops);
 
 let lastTime = 0;
-const FPS = 45;
+const FPS = 30;
 const step = 1000 / FPS;
 
-function draw(timestamp){
-    requestAnimationFrame(draw);
+function draw(timestamp) {
+  requestAnimationFrame(draw);
+  if (timestamp - lastTime < step) {
+    return;
+  }
+  lastTime = timestamp;
 
-    if(timestamp - lastTime < step) {return;};
-    lastTime = timestamp;
+  context.fillStyle = "rgba(0,0,0,0.06)";
+  context.fillRect(0, 0, canvasElement.width, canvasElement.height);
+  context.fillStyle = "#00ca00";
+  context.font = fontSize + "px monospace";
 
-    context.fillStyle = "rgba(0,0,0,0.05)"
-    context.fillRect(0,0,canvasElement.width,canvasElement.height)
-    context.fillStyle = "#00ca00"
-    context.font = fontSize + 'px monospace'
+  for (let i = 0; i < rainDrops.length; i++) {
+    const drop = rainDrops[i];
+    context.fillStyle = "#00ca00";
+    context.fillText(drop.prevChar, i * fontSize, (drop.idx - 1) * fontSize);
+    context.fillStyle = "#cef5ce";
+    context.fillText(drop.currentChar, i * fontSize, drop.idx * fontSize);
 
-    for (let i = 0; i < rainDrops.length; i++) {
-        const row = rainDrops[i];
-        const randomChar = chars.charAt(Math.floor(Math.random()*chars.length));
-        // context.fillStyle = "#cef5ce"
-        context.fillText(randomChar,i*fontSize,row * fontSize)
-        context.fillStyle = "#00ca00"
-        if(row > rows && Math.random() > 0.975){
-            rainDrops[i] = 0;
-        }else{
-            rainDrops[i]++;
-        }
+    const randomChar = chars.charAt(Math.floor(Math.random() * chars.length));
+
+    rainDrops[i].prevChar = rainDrops[i].currentChar;
+    rainDrops[i].currentChar = randomChar;
+
+    if (drop.idx > rows && Math.random() > 0.975) {
+      rainDrops[i].idx = 0;
+    } else {
+      rainDrops[i].idx++;
     }
-
+  }
 }
 
-requestAnimationFrame(draw)
+requestAnimationFrame(draw);
